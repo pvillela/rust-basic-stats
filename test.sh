4b8452ebd3555868652f0f5eda38aa2e4e9148f2
pvillela <pvillela@gmail.com>
2026-05-17 22:46:20 -0400

    Removed duplicate logic in test.sh.

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# NOCOVER environment variable enables tests that are excluded from test coverage measurement.
-
-NOCOVER="1" cargo nextest run --all-targets --all-features --target-dir target/test-target
-cargo test --doc
-
-#!/bin/bash
-
 export RUSTFLAGS="-Awarnings"
 
 # NOCOVER environment variable enables tests that are excluded from test coverage measurement.
